9ac17a0115cc2604c4d5f9327f5478ff3debcc74
Marco Aguilar <marco97aguilar@gmail.com>
2021-08-25 17:35:47 -0600

    modelss

--- Devgradu/settings.py
+++ Devgradu/settings.py
@@ -74,12 +74,17 @@ WSGI_APPLICATION = 'Devgradu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbdevgradu',
+        'USER': 'devgradu',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432,
+   }
+
 }
 """
 import dj_database_url
@@ -88,8 +93,9 @@ from decouple import config
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
+    
     )
-}
+}"""
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
--- apps/webgradu/models.py
+++ apps/webgradu/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class emisor(models.Model):
+    pk_emisor = models.AutoField(primary_key=True, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(max_length=8, null=False, blank=False)
+    comprob = models.CharField(max_length=9, null=False, blank=False)
+
+
+class receptor(models.Model):
+    pk_receptor = models.AutoField(primary_key=True, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(max_length=8, null=False, blank=False)
+    guia = models.CharField(max_length=9, null=False, blank=False)
+
+class mensajero(models.Model):
+    ps_mensajero= models.AutoField(primary_key=True, null=False, blank=False)
+    id_mensajero = models.CharField(max_length=5, null=False, blank=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    apellido = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(max_length=8, null=False, blank=False)
+    correo = models.CharField(max_length=40, null=False, blank=False)
+
+class paquete(models.Model):
+    pk_paquete = models.AutoField(primary_key=True, null=False, blank=False)
+    guia = models.CharField(max_length=9, null=False, blank=False)
+    descrip = models.TextField(null=False, blank=False)
+    ubication = models.CharField(max_length=80, null=False,blank=False)
+    peso = models.DecimalField(null=False, max_digits=2, decimal_places=2)
+    tipo = models.TextField(null=False,blank=False)
+    fk_emisor = models.ForeignKey(emisor, null=False, blank=False, on_delete=models.CASCADE)
+    fk_receptor = models.ForeignKey(receptor, null=False, blank=False, on_delete=models.CASCADE)
+    fk_mensajero = models.ForeignKey(mensajero, null=False, blank=False, on_delete=models.CASCADE)
+
+class seguimiento(models.Model):
+    pk_seguimiento  = models.AutoField(primary_key=True, null=False, blank=False)
+    fecha = models.CharField(max_length=8, null=False, blank=False)
+    ubicacion = models.TextField(max_length=80, null=False,blank=False)
+    estado = models.TextField(max_length=2, null=False,blank=False)
+    fk_paquete = models.ForeignKey(paquete, null=False, blank=False, on_delete=models.CASCADE)
+
+
